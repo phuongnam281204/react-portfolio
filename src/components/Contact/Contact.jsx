56ffd00a35ac4496290c2711b69cfb156292d2d4
phuongnam281204 <namthuhai2812@gmail.com>
2025-08-19 22:56:26 +0700

    Initial commit - setup project structure and assets
    - Added React components: Contact, Project, Experience, Hero, Navbar
    - Added styles and module CSS files
    - Added assets (icons, images)
    - Added JSON data files (history, projects, skills)

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:phuongnamtran2812@gmail.com">phuongnamtran2812@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.facebook.com/namphuongtran2812">facebook.com/namphuongtran2812</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/phuongnam281204">github.com/phuongnam281204</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
