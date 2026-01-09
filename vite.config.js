ffcfbb8a2926a4aadbd97e8977715f103d0652f1
phuongnam281204 <namthuhai2812@gmail.com>
2026-01-09 20:30:12 +0700

    update cách deloy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css:{
+  css: {
     modules: {
-      localsConvention: 'camelCase',
-  }
-}
-})
+      localsConvention: "camelCase",
+    },
+  },
+  base: "/react-portfolio/",
+});
